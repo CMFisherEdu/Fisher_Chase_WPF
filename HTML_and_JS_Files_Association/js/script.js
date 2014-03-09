b2f4ba23ea618f6dd23f2ce065ca3a8a32794733
Fisher Chase <CMFisherEdu@gmail.com>
2014-03-09 15:11:39 -0600

    Changes - index1 and script.js
    Added Code and Data for “Screencast: Prompt and Alert”

--- HTML_and_JS_Files_Association/js/script.js
+++ HTML_and_JS_Files_Association/js/script.js
@@ -1,1 +1,11 @@
 alert("This is in the js file");
+
+var Numeric_Input = prompt("Give me a number. \nI'll parrot it back to you!");
+console.log(Numeric_Input);
+alert("Okay, that was neat - let's try some math.");
+var width = prompt("Let's get the area of a rectangular object. \nPlease input a width.");
+var height = prompt("Please enter a height.");
+var area_rectangle = width * height;
+console.log(area_rectangle);
+
+alert("The area of this rectangle is " + area_rectangle);
