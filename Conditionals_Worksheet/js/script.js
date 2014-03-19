6edf7fb6d96803a3ada7acd5df988bddf6c8099d
Fisher Chase <CMFisherEdu@gmail.com>
2014-03-19 08:52:59 -0600

    created folder: conditionals
    Committed Folder

---
+++ Conditionals_Worksheet/js/script.js
@@ -0,0 +1,16 @@
+/*
+C. "Michael" Fisher
+19 March 2014
+Activity: Conditionals Worksheet
+ */
+
+// Stuff your face I
+
+var compWeight = 120;
+var hvyWeightQual = 210;
+if (compWeight >= hvyWeightQual){
+    console.log("The competitor qualifies for the heavyweight division.");
+}
+else{
+    console.log("The competitor needs to gain some weight.");
+}
